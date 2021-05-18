06f4e7fa0f617381a05f773b6e4bfe11d989ac40
Samuel Keusch <sixtisam@gmail.com>
2021-05-18 17:28:42 +0200

    Line endings

--- code/verkaufsanalyse_run.sh
+++ code/verkaufsanalyse_run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir build
+
 javac -classpath ${HADOOP_CLASSPATH} Verkaufsanalyse.java -d build
 jar -cvf team1MapReduce.jar -C build .
 
@@ -7,3 +9,4 @@ $HADOOP_PREFIX/bin/hdfs dfs -rm -r map-reduce-assignment/output/Verkaufsanalyse
 $HADOOP_PREFIX/bin/hadoop jar team1MapReduce.jar team1MapReduce.Verkaufsanalyse map-reduce-assignment/input/purchases.txt map-reduce-assignment/output/Verkaufsanalyse
 
 $HADOOP_PREFIX/bin/hdfs dfs -cat map-reduce-assignment/output/Verkaufsanalyse/*
+
