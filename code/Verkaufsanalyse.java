30e3b98be6db36a7d8800f483ce85065a148d0d0
Samuel Keusch <sixtisam@gmail.com>
2021-05-18 16:15:53 +0200

    Initial code

---
+++ code/Verkaufsanalyse.java
@@ -0,0 +1,89 @@
+package team1MapReduce;
+
+//== MinimalMapReduceExt: The simplest possible MapReduce driver, which shows the defaults and uses own Mapper and Reducer
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+//== these imports are new
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class Verkaufsanalyse extends Configured implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.printf("Usage: %s [generic options] <in> <out>\n", getClass().getSimpleName());
+			ToolRunner.printGenericCommandUsage(System.err);
+			return -1;
+
+		}
+		Job job = Job.getInstance(getConf(), "minimapredext");
+		job.setJarByClass(this.getClass());
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setMapperClass(MyMapper.class);
+		job.setMapOutputKeyClass(LongWritable.class);
+		job.setMapOutputValueClass(Text.class);
+
+		job.setPartitionerClass(HashPartitioner.class);
+
+		job.setNumReduceTasks(1);
+		job.setReducerClass(MyReducer.class);
+
+		job.setOutputKeyClass(LongWritable.class);
+		job.setOutputValueClass(Text.class);
+
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static class MyMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+		@Override
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+
+			/*
+			 * TODO implement
+			 */
+			context.write (key, value);
+		}
+	}
+
+	public static class MyReducer extends Reducer<LongWritable, Text, LongWritable, Text> {
+
+		@Override
+		public void reduce(LongWritable key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+
+			/*
+			 * TODO implement
+			 */
+			for (Text val : values){
+				context.write (key, new Text(val));
+			}
+		}
+	}	
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new  MinimalMapReduceExt(), args);
+		System.exit(res);
+	}
+}
